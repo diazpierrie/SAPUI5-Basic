a744df7ee785b1e2664360311fe624ee2a12aa6f
diazpierrie <ezequieldiazpierri@gmail.com>
2023-01-26 01:17:48 +0000

    Final

--- invoices/SAPUI5/webapp/controller/Details.controller.js
+++ invoices/SAPUI5/webapp/controller/Details.controller.js
@@ -16,6 +16,8 @@ sap.ui.define(
 
     return Controller.extend("logaligroup.SAPUI5.controller.Details", {
       _onObjectMatch: function (oEvent) {
+        this.byId("rating").reset();
+
         this.getView().bindElement({
           path:
             "/" +
@@ -42,6 +44,17 @@ sap.ui.define(
           const oRouter = UIComponent.getRouterFor(this);
           oRouter.navTo("RouteApp", {}, true);
         }
+      },
+
+      onRatingChange: function (oEvent) {
+        const fValue = oEvent.getParameter("value");
+        const oResourceBundle = this.getView()
+          .getModel("i18n")
+          .getResourceBundle();
+
+        sap.m.MessageToast.show(
+          oResourceBundle.getText("ratingConfirmation", [fValue])
+        );
       },
     });
   }
